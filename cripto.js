aa08bd5e484f5929064ba63cff8bcd80ad84eb24
Givaona <giovanabanana0@gmail.com>
2022-04-05 22:43:57 -0300

    passinho do romano start

---
+++ cripto.js
@@ -0,0 +1,8 @@
+var submit = document.querySelector("#submit")
+submit.addEventListener('click', function(event){
+    event.preventDefault();
+} )
+
+var texto = document.querySelector("#codigo")
+var arrTexto = texto.split("");
+
